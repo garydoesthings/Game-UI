3db71b80b44cd1b4ed7a0c4c73cca1570b59e6ef
Unknown <garydoesthings@gmail.com>
2018-06-25 14:34:37 -0600

    Submission Commit
    Uploading files for assignment submission.

---
+++ Assets/_Scripts/GameState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    public GameObject PauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         if (Input.GetKey("escape"))
+        {
+            this.Pause();
+        }
+             
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Pause()
+    {
+        PauseMenu.SetActive(true);
+    }
+}
---
+++ Assets/_Scripts/optionsLog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionsLog : MonoBehaviour {
+
+    public GameObject previousMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void openOptions(GameObject menu)
+    {
+        previousMenu = menu;
+        this.gameObject.SetActive(true);
+    }
+
+    public void closeOptions()
+    {
+        if (previousMenu)
+        {
+            previousMenu.SetActive(true);
+        }
+        this.gameObject.SetActive(false);
+    }
+
+
+}
